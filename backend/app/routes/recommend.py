aeeaa183443459284742bf84470ee7ab4e22bb10
Sanchita Kapoor <sanchitakapoor36@gmail.com>
2026-02-09 01:48:17 +0530

    updated files with codes

--- backend/app/routes/recommend.py
+++ backend/app/routes/recommend.py
@@ -1,7 +1,33 @@
 from fastapi import APIRouter, UploadFile, File
+import os, shutil
+
+from app.services.mediapipe_service import extract_landmarks
+from app.services.body_shape import classify_body_shape
 
 router = APIRouter(prefix="/recommend", tags=["Recommendation"])
 
+UPLOAD_DIR = "data/uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 @router.post("/")
 async def upload_image(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # ---- AI PROCESSING ----
+    landmarks = extract_landmarks(file_path)
+    if landmarks is None:
+        return {
+            "message": "No body detected",
+            "body_shape": None
+        }
+
+    body_shape = classify_body_shape(landmarks)
+
+    return {
+        "message": "Image processed",
+        "body_shape": body_shape,
+        "landmarks_count": len(landmarks)
+    }
---
+++ backend/app/services/mediapipe_service.py
@@ -0,0 +1,20 @@
+import cv2
+import mediapipe
+
+mp_pose = mediapipe.solutions.pose
+
+def extract_landmarks(image_path: str):
+    """Returns list of landmark objects or None"""
+    image = cv2.imread(image_path)
+    if image is None:
+        return None
+
+    with mp_pose.Pose(static_image_mode=True) as pose:
+        results = pose.process(
+            cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        )
+
+    if not results.pose_landmarks:
+        return None
+
+    return results.pose_landmarks.landmark
